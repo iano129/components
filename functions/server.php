039bb48847a7b4af415688be4881e3163a535e27
theoneskibid67 <lopesresende@gmail.com>
2026-09-17 15:14:55 -0300

    leo Update

--- functions/server.php
+++ functions/server.php
@@ -1,6 +1,12 @@
-<?php 
+<?php
 
-function servidorHttp(){
-    echo '1. Servidor HTTP recebeu a requisição. <br>';
-    router();
+function servidorHttp(){ 
+    echo '1. Servidor HTTP recebeu a requisição. <br>'; 
+    router(); 
 }
+
+function router(){
+    echo '2. Router recebeu a requisição. <br>';
+}
+
+servidorHttp();
